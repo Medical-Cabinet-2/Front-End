225cb7a2ddd60b12e066a7292a1b8f7f93a6a589
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-01-09 11:02:29 -0800

    Commit 6: black stuff added to rregister page

--- med-cab/src/components/login-form/logIn.js
+++ med-cab/src/components/login-form/logIn.js
@@ -28,14 +28,14 @@ const LogIn = ({ values, errors, touched, status, handleSubmit }) => {
                 <Form onSubmit={handleSubmit} className="formcont">
                     <FormGroup>
                     {/* E-Mail */}
-                    <label htmlFor = 'email'>E-Mail:</label>
+                    <label htmlFor = 'email' className="labels">E-Mail:</label>
                     <Field type = 'text' name = 'email' className="logfield"/>
                     </FormGroup>
                     {touched.email && errors.email && (<Errors>{errors.email} </Errors>)}
 
                     <FormGroup>
                     {/* Password */}
-                    <label htmlFor = 'password'>Password:</label>
+                    <label htmlFor = 'password' className="labels">Password:</label>
                     <Field type = 'password' name = 'password' className="logfield1"/>
                     </FormGroup>
                     {touched.password && errors.password && (<Errors>{errors.password} </Errors>)}
@@ -53,7 +53,7 @@ const LogIn = ({ values, errors, touched, status, handleSubmit }) => {
                 </RegisterLink>
 
                 <RegisterLink>
-                    <p><b>Forgot password? <a href = '#'>Click here.</a></b></p>
+                    <p>Forgot password? <a href = '#'>Click here.</a></p>
                 </RegisterLink>
 
             </FormLinks> 
