8286e83c124eec4bec5f006f260a36b6df893821
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-01-08 17:02:18 -0800

    Commit 12: Clean up

--- med-cab/src/components/dashboard/Display.js
+++ med-cab/src/components/dashboard/Display.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Col, Row, Button } from 'reactstrap';
+import { Col, Row, Button } from 'reactstrap';
 import "bootstrap/dist/css/bootstrap.css";
 import SavedStrainsList from '../recommendations/SavedStrainsList';
 import AppHeader from './AppHeader';
--- med-cab/src/store/reducers/strainReducer.js
+++ med-cab/src/store/reducers/strainReducer.js
@@ -101,6 +101,14 @@ const initialState = {
             img: "https://assets.wikileaf.com/assets/strains/strain/Lambs_Bread.jpg"
         },
         {
+            name: "Lamb's Bread",
+            type: "sativa",
+            rating: 4,
+            effects: "Cotton Mouth, Creative, Paranoia",
+            description: "Lamb’s Bread, Lamb’s Breath, Lambsbread - whatever the name the high is the same, with a rush of uplifting energy that hits you hard in the head and giving you a mood boost that is unparalleled. The euphoria and creativity spring to life after only a few puffs and will have you getting through an artist’s block with ease. This is a high that doesn’t want to slow down and will last for hours, so enjoy the stimulation as it takes you through the galaxy because you’ll feel out of this world. Its effects manage nausea, depressed and pain.",
+            img: "https://assets.wikileaf.com/assets/strains/strain/Lambs_Bread.jpg"
+        },
+        {
             name: "Jack the Ripper",
             type: "sativa",
             rating: 4.5,
